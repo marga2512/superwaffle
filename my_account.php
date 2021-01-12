47541a44a7e76a3593b414381dd29e70ea26597f
Kirby2020 <74135383+Kirby2020@users.noreply.github.com>
2021-01-12 14:32:44 +0100

    Add files via upload

--- my_account.php
+++ my_account.php
@@ -3,12 +3,12 @@
 
   if (!isset($_SESSION['username'])) {
     $_SESSION['msg'] = "You must log in first";
-    header('location: /php/login.php');
+    header('location: /registration/login.php');
   }
   if (isset($_GET['logout'])) {
     session_destroy();
     unset($_SESSION['username']);
-    header("location: /php/login.php");
+    header("location: /registration/login.php");
   }
 ?>
 
