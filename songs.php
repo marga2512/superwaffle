ca58e5179c0d492eb4bb314a6ff8b2ad7b941051
Kirby2020 <74135383+Kirby2020@users.noreply.github.com>
2021-01-12 14:26:27 +0100

    Update songs.php

--- songs.php
+++ songs.php
@@ -44,7 +44,7 @@ $items = $stmt->fetchAll(PDO::FETCH_ASSOC);
           <li><a href="index.html">Home</a></li>
           <li><a href="songs.php">Songs</a></li>
           <li><a href="playlists.html">Playlists</a></li>
-          <li><a href="my_account.html">My Account</a></li>
+          <li><a href="my_account.php">My Account</a></li>
         </ul>
       </nav>
     </header>
