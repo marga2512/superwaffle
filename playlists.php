d78dd3090c8ddacf7abeb08554e3e373f55dc8a0
Kirby2020 <74135383+Kirby2020@users.noreply.github.com>
2021-01-12 14:39:33 +0100

    Add files via upload

---
+++ playlists.php
@@ -0,0 +1,45 @@
+<?php 
+  session_start(); 
+
+  if (!isset($_SESSION['username'])) {
+    $_SESSION['msg'] = "You must log in first";
+    header('location: /registration/login.php');
+  }
+  if (isset($_GET['logout'])) {
+    session_destroy();
+    unset($_SESSION['username']);
+    header("location: /registration/login.php");
+  }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="./css/common.css">
+    <title>Playlists</title>
+  </head>
+
+  <body id="top">
+    <header>
+      <img id="logo" src="./img/logo.png" width="128">
+      <nav>
+        <ul>
+          <li><a href="index.html">Home</a></li>
+          <li><a href="songs.php">Songs</a></li>
+          <li><a href="playlists.php">Playlists</a></li>
+          <li><a href="my_account.php">My Account</a></li>
+        </ul>
+      </nav>
+    </header>
+
+    <main>
+      <h1>Log in to view your playlists</h1>
+    </main>
+
+    <footer>
+      <a href="#top">Back to top</a>
+    </footer>
+  </body>
+</html>
